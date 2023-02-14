0c054a032c850b668107e15143c1fef3b599f660
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2023-02-14 13:30:17 -0500

    Authentication Set-Up

--- SpotifyClone/SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/SpotifyClone/Managers/AuthManager.swift
@@ -11,7 +11,22 @@ final class AuthManager
 {
     static let shared = AuthManager()
     
+    struct Constants
+    {
+        static let clientID = "2cbf6c2ec7104e1cb1862dfd37419bfc"
+        static let clientSecret = "e9e619080c384d2bb8da6dd40fe8e046"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL?
+    {
+        let scopes = "user-read-private"
+        let redirectURI = "https://open.spotify.com/"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool
     {
@@ -28,9 +43,28 @@ final class AuthManager
         return nil
     }
     
+    private var tokenExpirationDate: Date?
+    {
+        return nil
+    }
+    
     private var shouldRefreshToken: Bool
     {
         return false
     }
     
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void)
+    {
+        // Get token
+    }
+    
+    private func cacheToken()
+    {
+        
+    }
+    
+    private func refreshAccessToken()
+    {
+        
+    }
 }
